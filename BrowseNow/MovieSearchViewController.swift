6520301fd493eb39cfae10f962fcd643d7cb5b1e
dipikabari <dipikab@y7mail.com>
2025-05-07 16:53:49 +0100

    Initial commit

---
+++ BrowseNow/MovieSearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovieSearchViewController.swift
+//  BrowseNow
+//
+//  Created by Dipika Bari on 07/05/2025.
+//
+
+import UIKit
+
+class MovieSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
